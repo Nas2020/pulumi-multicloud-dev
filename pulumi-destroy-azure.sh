a42db5688424673fd6bda1888114e973580a322f
Nas <59632614+Nas2020@users.noreply.github.com>
2025-03-03 20:27:16 -0500

    refactor: removed unnecessary files and Included Azure README

--- pulumi-destroy-azure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export ARM_CLIENT_ID=""
-export ARM_CLIENT_SECRET=""
-export ARM_TENANT_ID=""
-export ARM_SUBSCRIPTION_ID=""
-
-# Run pulumi command with the provided arguments
-# pulumi "$@"
-pulumi destroy
